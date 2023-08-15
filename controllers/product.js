1f5c990e162a2cf71e3a90636f60f54ed1c79829
Kevin Neema Brian <kevinbrian@cortex-moz.com>
2023-08-15 08:00:30 +0200

    📦 NEW: getProducts method

--- controllers/product.js
+++ controllers/product.js
@@ -1,21 +1,23 @@
-const User = require('../model/user')
-//const Product = require('../model/product')
+const User = require("../model/user");
+const Product = require("../model/product");
 
 const getProducts = async (req, res) => {
-    
+  try {
     // Write the code to get the product details
-
-}
+    const results = Product.find({});
+    
+    res.status(200).send(results);
+  } catch (error) {
+    return res.status(500).send(error?.message);
+  }
+};
 
 const addProduct = async (req, res) => {
-    
-    // Write the code to add the product details
-}
+  // Write the code to add the product details
+};
 
 const deleteProduct = async (req, res) => {
-    
-    // Write the code to delete the product details
-
-}
+  // Write the code to delete the product details
+};
 
 module.exports = { getProducts, addProduct, deleteProduct };
